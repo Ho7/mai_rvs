674f24ff11634673e30644ff66c4d573519daa4c
ho7 <nyuokunev@avito.ru>
2019-12-03 21:09:50 +0300

    kek

--- service_layler.py
+++ service_layler.py
@@ -9,13 +9,23 @@ class OneLessThatItWasException(Exception):
     code = 2
 
 
-def processing(db: redis.Redis, number: int) -> int:
-    pipeline = db.pipeline(transaction=True)
-    if pipeline.get(number):
+def _do(p: redis.client.Pipeline, number):
+    p.watch(number)
+    if p.get(number):
         raise AlreadyExistException('Number already exists')
-    elif pipeline.get(number + 1):
+    elif p.get(number + 1):
         raise OneLessThatItWasException('Number is 1 less than required')
+    p.execute()
+    p.unwatch()
+    p.set(number, 'True')
+    p.execute()
 
-    pipeline.set(number, 'True')
+
+def processing(db: redis.Redis, number: int) -> int:
+    with db.pipeline(transaction=True) as p:
+        try:
+            _do(p, number)
+        except redis.WatchError:
+            _do(p, number)
 
     return number + 1
